7a6efe692e3c4693659466879e205873c73009a2
Rumeysa Başaran <rumeysa.basaran@gmail.com>
2022-09-16 14:30:20 +0300

    donguler

---
+++ Odev6/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //for
+        //tek sayıları yazdırmak için i = 1 : i+=2
+        //çift sayıları yazdırmak için i = 2 : i+=2
+        for(int i=1;i<10;i+=2){
+            System.out.println(i);
+        }
+        System.out.println("Döngü Bitti");
+    }
+}
