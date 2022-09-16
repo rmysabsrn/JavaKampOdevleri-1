7971250d13179c7797239b42ddf9cd7529797290
Rumeysa Başaran <rumeysa.basaran@gmail.com>
2022-09-16 13:39:42 +0300

    datataypes

---
+++ Odev3/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 10.50;
+        char c = 'T';
+        boolean d = false;
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
